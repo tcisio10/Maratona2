a8ba2158d177ae717057ed9bdaaacd074911c699
tcisio10 <63074263+tcisio10@users.noreply.github.com>
2020-06-07 23:36:49 -0300

    1156

---
+++ 1156.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class MyClass {
+	
+    public static void main(String[] args) throws IOException {
+    	double S = 1;
+    	int cont = 2;
+    	for (int i = 3; i <= 39; i++) {
+    		if (i % 2 == 1)  {
+    			S += (double) i / cont;
+        		cont *= 2.00;
+    		}
+    	}
+    	System.out.println(String.format("%.2f", S));
+    }
+	
+}
